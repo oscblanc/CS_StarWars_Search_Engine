ffaf76fa2da8b956d6c87fa4e5e458417b327eb8
Oscar Blanco <oblancomartinez@gmail.com>
2022-10-04 11:23:27 -0500

    Cards desplegadas oct04

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function search_character_button_click() {
       <span class="cardText">Height: </span><span>${character.height}</span><br />
       <span class="cardText">Hair color: </span><span>${character.hair_color}</span><br />
       <span class="cardText">Skin color: </span><span>${character.skin_color}</span><br />
+      <img src="${character.photo}" alt="${character.photo}" class="Photos">
     </div>
   `
 }
@@ -30,14 +31,27 @@ function search_character(character_name) {
   }
 }
 
-class Character {
-  constructor(name, gender) {
-    
-    
+let http= new XMLHttpRequest();
+http.open('get','swapi.js',true);
+http.send();
+http.onload=function(){
+  if(this.readyState == 4 && this.status == 200){
+    let products = JSON.parse(file).results;
+    let output = "";
+    for(let item of products){
+      output += `
+      <div class="menuContainer">
+        <span class="cardText">Name: </span><span>${item.name}</span><br />
+        <span class="cardText">Birthday: </span><span>${item.birth_year}</span><br />
+        <span class="cardText">Gender: </span><span>${item.gender}</span><br />
+        <span class="cardText">Height: </span><span>${item.height}</span><br />
+        <span class="cardText">Hair color: </span><span>${item.hair_color}</span><br />
+        <span class="cardText">Skin color: </span><span>${item.skin_color}</span><br />
+        <img src="${item.photo}" alt="${item.photo}" class="Photos">
+      </div>
+    `
+
+    }
+    document.querySelector(".products").innerHTML = output;
   }
-  
-  obtener_iniciales() {
-    
-  }
-  
 }
